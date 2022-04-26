5e7fe785421d2a34f64974e98eef046db3ec25a7
Davi Fernandes <davifernandessrn@gmail.con>
2022-04-26 13:53:50 -0300

    Add app.js

--- Fluxo de Caixa/src/index.js
+++ Fluxo de Caixa/src/index.js
@@ -1,33 +1,7 @@
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const bodyParser = require('body-parser')
+import app from './src/app.js'
 
-const app = express()
+const port = process.env.PORT || 8000;
 
-app.use(morgan('dev'))
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(express.json())
-app.use(cors())
-
-
-app.listen(3000, () =>{
-    console.log(`Express started at http:://localhost:3000`)
-})
-
- 
-
-//buscardados
- app.get('/', (req, res) => {
-    return res.json(db)
-  })
-
-  //inserir dados
-  app.post('/', (req,res) =>{
-    const body = req.body
-    if(!body)
-    return res.status(400).end()
-
-    db.push(body)
-    return res.json(body)
+app.listen(port, () => {
+    console.log(`servidor escutando em http://localhost:${port}`)
 })
