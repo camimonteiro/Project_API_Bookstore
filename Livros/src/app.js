073cfeff5c0aeee0310acda45849fd686ee2ad03
Camille Monteiro <camillemonteiro.dev@gmail.com>
2022-04-27 16:00:31 -0300

    feat: file update to import controller, model and bd

--- Livros/src/app.js
+++ Livros/src/app.js
@@ -5,7 +5,19 @@ const port = 3000;
 // Body-parse
 app.use(express.json());
 
+// Importando Controller
+const livrosController = require('./controller/livros.controller');
+
+// Importação do modelo criado
+const livrosModel = require('./model/livros.model');
+
+// Importando o banco de dados criado
+const bd = require('./infra/bd');
+
+livrosController(app, bd);
+
 // Rodando o servidor na porta indicada
 app.listen(port, function() {
     console.log('Online! rodando na porta 3000')
 })
+
