bcd3c719176048e9132a77578af3d19eec55fa50
Camille Monteiro <camillemonteiro.dev@gmail.com>
2022-05-01 20:02:25 -0300

    fix: correction of data in the database and in the file with data. Change in ISBN attribute type from integer to varchar

--- Livros/src/infra/criaBanco.js
+++ Livros/src/infra/criaBanco.js
@@ -14,16 +14,16 @@ CREATE TABLE IF NOT EXISTS "LIVROS" (
     "ID_FK_FORNECEDORES" INTEGER,
     "NOME_FK_FORNECEDORES" varchar (64),
     "PRECO" DECIMAL,
-    "ISBN" INTEGER,
+    "ISBN" varchar(13),
     "AVALIACAO" DECIMAL
   );`;
 
 const ADD_LIVROS_DATA = `
 INSERT INTO LIVROS (ID, TITULO, AUTOR, SINOPSE, CATEGORIA, EDICAO, QTDPAGINAS, ID_FK_FORNECEDORES, NOME_FK_FORNECEDORES, PRECO, ISBN, AVALIACAO)
 VALUES 
-    (1, 'Harry Potter e a Pedra Filosofal', 'J.K. Rowling', 'teste sinopse', 'Fantasia de Ação e Aventura', '1ª Edição', 208, 2, 'Editora Rocco', 24.90, 9788532530783, 4.9),
-    (2, 'Mulheres que correm com os lobos', 'Clarissa Pinkola Estés', 'teste sinopse 2', 'Inspiradores', '1ª Edição', 576, 2, 'Editora Rocco', 34.90, 9788532529787, 4.8),
-    (3, 'A coragem de ser imperfeito', 'Brené Brown', 'teste sinopse 3', 'Motivacional Auto-Ajuda', '1ª Edição', 208, 3, 'Editora Sextante', 29.90, 9788543104331, 4.8)
+    (1, 'Harry Potter e a Pedra Filosofal', 'J.K. Rowling', 'teste sinopse', 'Fantasia de Ação e Aventura', '1ª Edição', 208, 2, 'Editora Rocco', 24.90, "9788532530783", 4.9),
+    (2, 'Mulheres que correm com os lobos', 'Clarissa Pinkola Estés', 'teste sinopse 2', 'Inspiradores', '1ª Edição', 576, 2, 'Editora Rocco', 34.90, "9788532529787", 4.8),
+    (3, 'A coragem de ser imperfeito', 'Brené Brown', 'teste sinopse 3', 'Motivacional Auto-Ajuda', '1ª Edição', 208, 3, 'Editora Sextante', 29.90, "9788543104331", 4.8)
 `
 
 function criaTabelaLivros() {
