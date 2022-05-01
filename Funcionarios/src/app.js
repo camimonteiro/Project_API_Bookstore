4156022bde4a0289a0d82375caf3737b313922a9
Emilly Costa <96596496+theemillycosta@users.noreply.github.com>
2022-05-01 18:25:06 -0300

    Delete Funcionarios directory

--- Funcionarios/src/app.js
+++
@@ -1,9 +0,0 @@
-const express = require ('express');
-const app = express(); 
-const port = 3000;
-
-const FuncionariosController = require('./controllers/funcionarios-controller')
-FuncionariosController (app)
-
-//rodar o servidor na porta
-app.listen(port, () => console.log(`Rodando na porta ${port}`))
