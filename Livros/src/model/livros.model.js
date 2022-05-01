7e1ee33e0fa907f992382ed5d00e2bf1848c6c63
Camille Monteiro <camillemonteiro.dev@gmail.com>
2022-05-01 18:10:05 -0300

    fix: data correction in the model file. Removal of the customers attribute, among other adjustments

--- Livros/src/model/livros.model.js
+++ Livros/src/model/livros.model.js
@@ -1,23 +1,20 @@
-var id = 0;
 class Livros {
-    constructor(titulo, autor, sinopse, categoria, edicao, qtdPaginas, idFornecedores, nomeFornecedores, preco, isbn, idClientes, avaliacao){
-        this.id = ++id;
+    constructor(titulo, autor, sinopse, categoria, edicao, qtdPaginas, id_FK_Fornecedores, nome_FK_Fornecedores, preco, isbn, avaliacao){
         this.titulo = titulo;
         this.autor = autor;
         this.sinopse = sinopse;
         this.categoria = categoria;
         this.edicao = edicao;
         this.qtdPaginas = qtdPaginas;
-        this.idFornecedores = idFornecedores;
-        this.nomeFornecedores = nomeFornecedores; 
+        this.id_FK_Fornecedores = id_FK_Fornecedores;
+        this.nome_FK_Fornecedores = nome_FK_Fornecedores; 
         this.preco = preco;
         this.isbn = this.validarISBN(isbn);
-        this.idClientes = idClientes;
         this.avaliacao = avaliacao;
     }
 
     validarISBN(isbn){
-        if (isbn.length == 13 ){
+        if (isbn.length === 13 ){
             return isbn;
         } else {
             throw new Error('ISBN code must have 13 numbers')
